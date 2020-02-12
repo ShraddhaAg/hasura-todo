9e5b3ef0deb06319ca6ad56ae2e95e0a4c5c2ded
Shraddha Agrawal <shraddha.agrawal000@gmail.com>
2020-02-12 23:25:48 +0530

    Resolve build errors

---
+++ build/precache-manifest.491ccad80a96f43e228eca1d07b81f7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "168fe125c838ef8a60356382e1829983",
+    "url": "/static/media/puff.168fe125.svg"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "103ff9d7a897da3ebc8a44c5e8877509",
+    "url": "/static/media/index.103ff9d7.mjs"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3eee95abb7fbb73035df",
+    "url": "/static/js/main.3eee95ab.chunk.js"
+  },
+  {
+    "revision": "36be686350231087f6ee",
+    "url": "/static/js/1.36be6863.chunk.js"
+  },
+  {
+    "revision": "3eee95abb7fbb73035df",
+    "url": "/static/css/main.be324ef1.chunk.css"
+  },
+  {
+    "revision": "be10e4fba4460bf0bfd22e5f3493eb4d",
+    "url": "/index.html"
+  }
+];
--- src/env.js
+++ src/env.js
@@ -2,5 +2,5 @@ export const vars = {
     "GRAPHQL_ENDPOINT": 'https://shraddhaag-todo.herokuapp.com/v1/graphql',
     "DOMAIN": "shraddhaag.auth0.com", //app.autho.com
     "CLIENT_ID": "3q6jY11RBz5BKF2YoBKxcF7Ep8vKcsfx",
-    "CALLBACK_URL": "http://localhost:3000/callback"
+    "CALLBACK_URL": "https://shraddhaag-todo-react.herokuapp.com/callback"
   }
